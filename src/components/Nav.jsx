38e8c128244990361c711cab6e3799a9f334378b
Jay Mingle <jkawudi@gmail.com>
2024-01-31 03:56:02 +0000

    Changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     return (
         <div className="nav nav_black">
             <div className="nav_contents">
-                <img className="nav_logo" src="https://image.similarpng.com/very-thumbnail/2020/11/Netflix-logo-design-illustration-on-transparent-background-PNG.png" alt="Netflix logo"/>
+                <img className="nav_logo" src="https://pngimg.com/uploads/netflix/netflix_PNG17.png" alt="Netflix logo"/>
                 <img className="nav_avatar" src="https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-vnl1thqrh02x7ra2.webp" alt="User Avatar"/>
             </div>
         </div>
