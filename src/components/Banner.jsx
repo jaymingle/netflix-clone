f65c802e9af30964c8dfe02e6de6cbc9651dd688
Jay Mingle <jkawudi@gmail.com>
2024-01-31 11:58:07 +0000

    Changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,6 +6,7 @@ const Banner = () => {
         <header className="banner" style={{
             backgroundImage: `url("https://cdn.freebiesupply.com/logos/large/2x/netflix-logo-png-transparent.png")`,
             backgroundSize: 'cover',
+            backgroundPosition: "center center"
         }}>
             {/*<img src="https://wallpapercave.com/wp/wp1917118.jpg" alt="Netflix Banner Image" className=""/>*/}
         </header>
