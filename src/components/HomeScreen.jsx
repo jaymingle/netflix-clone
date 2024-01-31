f1f6f5aff0e8b3870b6bc29d372b875bbdd8e858
Jay Mingle <jkawudi@gmail.com>
2024-01-31 04:12:35 +0000

    Changes

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -5,67 +5,7 @@ const HomeScreen =  () => {
     return(
         <div className="homeScreen">
             <Nav/>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
-            <h1>The Test</h1>
+
         </div>
     )
 }
