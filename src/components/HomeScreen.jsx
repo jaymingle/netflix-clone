6ff3cb8d8d40a7228bef11956be036df968d54fc
Jay Mingle <jkawudi@gmail.com>
2024-01-31 03:04:21 +0000

    Changes

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,9 +1,11 @@
 import './HomeScreen.css'
+import Nav from "./Nav.jsx";
 
 const HomeScreen =  () => {
     return(
         <div className="homeScreen">
             This is HomeScreen
+            <Nav/>
         </div>
     )
 }
