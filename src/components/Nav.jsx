16d81920ab014bc14ac1a7fb1913e7b0fa50b48d
Jay Mingle <jkawudi@gmail.com>
2024-01-31 03:41:39 +0000

    Changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,9 @@ import './Nav.css'
 
 const Nav = () => {
     return (
-        <div className="nav">
+        <div className="nav nav_black">
             <div className="nav_contents">
-                <img className="nav_logo" src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" alt="Netflix logo"/>
+                <img className="nav_logo" src="https://image.similarpng.com/very-thumbnail/2020/11/Netflix-logo-design-illustration-on-transparent-background-PNG.png" alt="Netflix logo"/>
                 <img className="nav_avatar" src="https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-vnl1thqrh02x7ra2.webp" alt="User Avatar"/>
             </div>
         </div>
