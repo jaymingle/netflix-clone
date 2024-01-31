edc242145eea789700a88033f09df3d881f802f2
Jay Mingle <jkawudi@gmail.com>
2024-01-31 11:57:15 +0000

    Changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,12 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div>
-            <img src="https://wallpapercave.com/wp/wp1917118.jpg" alt="Netflix Banner Image" className=""/>
-        </div>
+        <header className="banner" style={{
+            backgroundImage: `url("https://cdn.freebiesupply.com/logos/large/2x/netflix-logo-png-transparent.png")`,
+            backgroundSize: 'cover',
+        }}>
+            {/*<img src="https://wallpapercave.com/wp/wp1917118.jpg" alt="Netflix Banner Image" className=""/>*/}
+        </header>
     );
 };
 
