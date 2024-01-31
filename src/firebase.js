3f7fe1343c89a0fba7974ccce0807ea5c9ceb929
Jay Mingle <jkawudi@gmail.com>
2024-01-31 02:41:30 +0000

    Changes

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwC2riT8AsG9VdZu7Oqyi0P4zEYQ9Vj0c",
+    authDomain: "netflix-clone-app-24694.firebaseapp.com",
+    projectId: "netflix-clone-app-24694",
+    storageBucket: "netflix-clone-app-24694.appspot.com",
+    messagingSenderId: "488113113965",
+    appId: "1:488113113965:web:9299af95de0df67b99a864"
+};
