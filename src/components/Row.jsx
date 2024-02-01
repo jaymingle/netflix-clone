45d67cd9682e2b52908cdbda32a08d4a0c3ef881
Jay Mingle <jkawudi@gmail.com>
2024-02-01 03:09:01 +0000

    Changes

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./Row.css"
-const Row = () => {
+const Row = ({title, fetchUrl, isLargeRow}) => {
     return (
         <div className="row">
-
+            <h2 className="row-title">{title}</h2>
         </div>
     );
 };
