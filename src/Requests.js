70915c1c214b65fe4110bea0471a18ca06670ca1
Jay Mingle <jkawudi@gmail.com>
2024-02-01 01:35:11 +0000

    Changes

--- src/Requests.js
+++ src/Requests.js
@@ -3,12 +3,12 @@ const API_KEY = "c42c8f59d63952f7b8466fd374ff1dfb";
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=S{API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=S(API_KEY}&Language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&Language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=S{API_KEY}&with_genres=35`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=s{API_KEY}&with_genres=10749`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
 
