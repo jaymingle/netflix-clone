56a48ff0d22d75ffc41d5ebed75dfa662d11e1b3
Jay Mingle <jkawudi@gmail.com>
2024-02-01 00:02:35 +0000

    Changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -20,7 +20,26 @@ const Banner = () => {
                     <button className="banner_button">Play</button>
                     <button className="banner_button">My List</button>
                 </div>
-                <h1 className="banner_description">This is a test description. This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.</h1>
+                <h1 className="banner_description">
+                    This is a test description. This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.
+                    This is a test description.This is a test description.This is a test description.</h1>
             </div>
             <div className="banner-fadeBottom"/>
         </header>
