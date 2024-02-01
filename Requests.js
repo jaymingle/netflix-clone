da7014b0735e6045ee8f02d5535b4a3076b17ec0
Jay Mingle <jkawudi@gmail.com>
2024-02-01 01:23:54 +0000

    Changes

--- Requests.js
+++ Requests.js
@@ -11,4 +11,6 @@ const requests = {
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
 
+export default requests;
+
 //`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`
