bf17274172b093868a19d19a4619ddc71d271739
Jay Mingle <jkawudi@gmail.com>
2024-02-01 22:37:24 +0000

    Changes

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "./Row.css"
 const Row = ({title, fetchUrl, isLargeRow}) => {
+
+    const [movies, setMovies] = useState('')
+
+    useEffect(() =>{
+
+        
+
+    }, [])
+
+
+
     return (
         <div className="row">
             <h2 className="row-title">{title}</h2>
