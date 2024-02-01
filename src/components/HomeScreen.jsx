eb4ea1f20d09bbf3918b6f929d033ae30188712c
Jay Mingle <jkawudi@gmail.com>
2024-02-01 03:06:50 +0000

    Changes

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -2,17 +2,26 @@ import './HomeScreen.css'
 import Nav from "./Nav.jsx";
 import Banner from "./Banner.jsx";
 import Row from "./Row.jsx";
+import requests from "../Requests.js";
 
 const HomeScreen =  () => {
     return(
         <div className="homeScreen">
             <Nav/>
             <Banner/>
-            <Row/>
-            <Row/>
-            <Row/>
-            <Row/>
-            <Row/>
+
+            <Row
+                title="NETFLIX ORIGINALS"
+                fetchUrl={requests.fetchNetflixOriginals}
+                isLargeRow
+            />
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+            <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+            <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+            <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+            <Row title="Horror Movies" fetchUrl={requests. fetchHorrorMovies} />
+            <Row title="Romance Movies" fetchUrl={requests. fetchRomanceMovies} />
+            <Row title="Documentaries" fetchUrl={requests. fetchDocumentaries} />
         </div>
     )
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import "./Row.css"
 const Row = () => {
     return (
-        <div className="">
+        <div className="row">
 
         </div>
     );
