2cbfadd50af4ecabac26dd50b7098b7033934c76
Jay Mingle <jkawudi@gmail.com>
2024-02-01 23:51:21 +0000

    Changes

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -26,9 +26,11 @@ const Row = ({title, fetchUrl, isLargeRow = false}) => {
     return (
         <div className="row">
             <h2 className="row-title">{title}</h2>
-            {movies.map(movie => (
-                <img className={`row_poster ${isLargeRow && "row_posterLarge" }`} key={movie.id} src={`${baseURL}${isLargeRow ? movie?.poster_path : movie.backdrop_path}`} alt={movie?.name}/>
-            ))}
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img className={`row_poster ${isLargeRow && "row_posterLarge" }`} key={movie.id} src={`${baseURL}${isLargeRow ? movie?.poster_path : movie.backdrop_path}`} alt={movie?.name}/>
+                ))}
+            </div>
         </div>
     );
 };
