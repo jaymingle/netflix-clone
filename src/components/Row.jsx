da7f775e1d7a8bf9f9bcc0bd813421d31ea3c372
Jay Mingle <jkawudi@gmail.com>
2024-02-02 00:35:36 +0000

    Changes

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import "./Row.css"
 import axios from '../axios'
-import requests from "../Requests";
 const Row = ({title, fetchUrl, isLargeRow = false}) => {
 
     const [movies, setMovies] = useState([])
