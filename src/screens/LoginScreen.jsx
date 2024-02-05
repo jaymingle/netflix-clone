82a4ee1e167c1040e9751c395edec5281f63fd47
Jay Mingle <jkawudi@gmail.com>
2024-02-05 11:22:28 +0000

    Changes

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -6,9 +6,10 @@ const LoginScreen = () => {
         <div className="loginScreen">
             <div className="loginScreen_background">
                 <img className="loginScreen_logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="Netflix Logo"/>
+                <button className="loginScreen_button">Sign In</button>
+                <div className="loginScreen_gradient"/>
             </div>
-            <button className="loginScreen_button">Sign In</button>
-            <div className="loginScreen_gradient"></div>
+
         </div>
     );
 };
