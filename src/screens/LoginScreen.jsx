0242778859a153a447ad6362e55bd12436b1241a
Jay Mingle <jkawudi@gmail.com>
2024-02-05 10:30:07 +0000

    Changes

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -4,7 +4,9 @@ import "./LoginScreen.css"
 const LoginScreen = () => {
     return (
         <div className="loginScreen">
-            <div className="loginScreen_background"></div>
+            <div className="loginScreen_background">
+                <img className="loginScreen_logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="Netflix Logo"/>
+            </div>
         </div>
     );
 };
