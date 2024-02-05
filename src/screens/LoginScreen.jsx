01375f399f6451ebfd83045eb7c1273ce6248f9e
Jay Mingle <jkawudi@gmail.com>
2024-02-05 11:52:15 +0000

    Changes

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -14,6 +14,12 @@ const LoginScreen = () => {
                     <h1>Unlimited films, programmes and more.</h1>
                     <h2>Watch anywhere. cancel at anytime</h2>
                     <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                    <div className="loginScreen_input">
+                        <form>
+
+                        </form>
+                    </div>
+
                 </>
             </div>
 
