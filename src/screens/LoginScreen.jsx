ac18bead2feb327c78ee7740206fc8e27bf9e59d
Jay Mingle <jkawudi@gmail.com>
2024-02-11 04:14:59 +0000

    Changes

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -19,29 +19,21 @@ const LoginScreen = () => {
                 <div className="loginScreen_gradient"/>
             </div>
             {
-                signIn && <div className="loginScreen_body">
-                    <h1>Unlimited films, programmes and more.</h1>
-                    <h2>Watch anywhere. cancel at anytime</h2>
-                    <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
-                    <div className="loginScreen_input">
-                        <form>
-                            <input type="email" placeholder="Email Address"/>
-                            <button className="loginScreen_getStarted" onClick={signInHandler}>GET STARTED</button>
-                        </form>
+                signIn && (
+                    <div className="loginScreen_body">
+                        <h1>Unlimited films, programmes and more.</h1>
+                        <h2>Watch anywhere. cancel at anytime</h2>
+                        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                        <div className="loginScreen_input">
+                            <form>
+                                <input type="email" placeholder="Email Address"/>
+                                <button className="loginScreen_getStarted" onClick={signInHandler}>GET STARTED</button>
+                            </form>
+                        </div>
                     </div>
-                </div>            <div className="loginScreen_body">
-                <h1>Unlimited films, programmes and more.</h1>
-                <h2>Watch anywhere. cancel at anytime</h2>
-                <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
-                <div className="loginScreen_input">
-                <form>
-                <input type="email" placeholder="Email Address"/>
-                <button className="loginScreen_getStarted" onClick={signInHandler}>GET STARTED</button>
-</form>
-</div>
-</div>
-
+                )
             }
+
 
         </div>
     );
