dd4a84cd49aa608a3effb066786717e7c1998400
Jay Mingle <jkawudi@gmail.com>
2024-02-11 04:09:46 +0000

    Changes

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./LoginScreen.css"
 
 const LoginScreen = () => {
+
+    const [signIn, setSignIn] = useState(true)
+
+    const signInHandler = () => {
+        setSignIn(value => !value)
+    }
+
+    console.log(signIn)
+
     return (
         <div className="loginScreen">
             <div className="loginScreen_background">
                 <img className="loginScreen_logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="Netflix Logo"/>
-                <button className="loginScreen_button">Sign In</button>
+                <button className="loginScreen_button" onClick={signInHandler}>Sign In</button>
                 <div className="loginScreen_gradient"/>
             </div>
             <div className="loginScreen_body">
