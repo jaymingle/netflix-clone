64718bc411426e8202f166b7d6d2697b17e24c64
Jay Mingle <jkawudi@gmail.com>
2024-02-12 22:09:50 +0000

    Changes

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SignUpScreen.css'
 
 const SignUpScreen = () => {
+
+    const [usermail, setUsermail] = useState('')
+    const [userpassword, setUserpassword] = useState('')
 
     const registerHandler = e => {
         e.preventDefault();
@@ -11,14 +14,16 @@ const SignUpScreen = () => {
     const signInHandler = e => {
         e.preventDefault();
         alert('Signing in')
+        console.log(usermail, userpassword)
+
     }
 
     return (
         <div className="signupScreen">
             <h1>Sign In</h1>
             <form onSubmit={signInHandler} className="signupScreen_form">
-                <input type="email" placeholder="Email" required/>
-                <input type="password" placeholder="Password" required/>
+                <input type="email" onChange={e => setUsermail(e.target.value)} value={usermail} placeholder="Email" required/>
+                <input type="password" onChange={e => setUserpassword(e.target.value)} value={userpassword} placeholder="Password" required/>
                 <button type="submit">Sign In</button>
             </form>
             <h4> <span className="signupScreen_grey">New to Netflix?</span> <span className="signupScreen_link" onClick={registerHandler}> Sign up now. </span></h4>
