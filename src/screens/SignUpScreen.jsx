9fff23eea96c71a5bbccfa24edb66e9e9b0921b6
Jay Mingle <jkawudi@gmail.com>
2024-02-12 16:18:54 +0000

    Changes

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -3,19 +3,20 @@ import './SignUpScreen.css'
 
 const SignUpScreen = () => {
 
-    const formSubmitHandler = () => {
+    const formRegisterHandler = e => {
+        e.preventDefault();
 
     }
 
     return (
         <div className="signupScreen">
             <h1>Sign In</h1>
-            <form onSubmit={formSubmitHandler} className="signupScreen_form">
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
+            <form onSubmit={formRegisterHandler} className="signupScreen_form">
+                <input type="email" placeholder="Email" required/>
+                <input type="password" placeholder="Password" required/>
                 <button type="submit">Sign In</button>
             </form>
-            <h4>New to Netflix? <span>Sign up now.</span></h4>
+            <h4> <span className="signupScreen_grey">New to Netflix?</span> <span className="signupScreen_link"> Sign up now. </span></h4>
         </div>
     );
 };
