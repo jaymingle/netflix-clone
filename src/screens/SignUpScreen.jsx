bf2e56760a1fa4565b933a17e4b985989b86d133
Jay Mingle <jkawudi@gmail.com>
2024-02-13 00:02:44 +0000

    Changes

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import {auth} from "../../firebase";
 import './SignUpScreen.css'
 
@@ -9,15 +9,10 @@ const SignUpScreen = () => {
     const [usermail, setUsermail] = useState('')
     const [userpassword, setUserpassword] = useState('')
 
-    const registerHandler = e => {
-        e.preventDefault();
-        alert('Registered')
-    }
-
-    const signInHandler = e => {
+    const registerInHandler = e => {
         e.preventDefault();
 
-        if(userpassword.length <= 6){
+        if(userpassword.length <= 5){
             alert('Password must be at least 6 characters')
             return
         }
@@ -30,15 +25,19 @@ const SignUpScreen = () => {
         setUserpassword('')
     }
 
+    const signInHandler = e => {
+        e.preventDefault();
+    }
+
     return (
         <div className="signupScreen">
             <h1>Sign In</h1>
-            <form onSubmit={signInHandler} className="signupScreen_form">
+            <form onSubmit={registerInHandler} className="signupScreen_form">
                 <input type="email" onChange={e => setUsermail(e.target.value)} value={usermail} placeholder="Email" required/>
                 <input type="password" onChange={e => setUserpassword(e.target.value)} value={userpassword} placeholder="Password" required/>
                 <button type="submit">Sign In</button>
             </form>
-            <h4> <span className="signupScreen_grey">New to Netflix?</span> <span className="signupScreen_link" onClick={registerHandler}> Sign up now. </span></h4>
+            <h4> <span className="signupScreen_grey">New to Netflix?</span> <span className="signupScreen_link" onClick={signInHandler}> Sign up now. </span></h4>
         </div>
     );
 };
