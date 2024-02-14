0ee8a53636886749f84711cef0cb81c928b73ca9
Jay Mingle <jkawudi@gmail.com>
2024-02-14 03:48:08 +0000

    Changes

--- src/components/ProfileScreen.jsx
+++ src/components/ProfileScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProfileScreen = () => {
     return (
-        <div>
+        <div className="profileScreen">
             <h1>I am the Profile Screen</h1>
         </div>
     );
