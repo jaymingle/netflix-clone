404e5e160acbaa6683bbd7880af328e70f0d03c3
Jay Mingle <jkawudi@gmail.com>
2024-02-15 01:05:08 +0000

    Changes

--- src/components/PlanScreen.jsx
+++ src/components/PlanScreen.jsx
@@ -1,11 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import './PlanScreen.css'
+import db from "../../firebase.js";
+
+
 const PlanScreen = () => {
 
     const [products, setProducts] = useState([])
 
     useEffect(() => {
+        db.collection('products')
+            .where('active', '==', true)
+            .get()
+            .then((querySnapshot) => {
+                const products = {}
+                querySnapshot.forEach(async productDoc => {
+                    products[productDoc.id] = productDoc.data()
+                    const priceSnap = await productDoc.ref.collection("prices").get()
+                    priceSnap.docs.forEach(price => {
+                        products[productDoc.id].prices = {
+                            priceId: price.id,
+                            priceData: price.data(),
+                        }
+                    })
 
+                })
+                setProducts(products)
+        })
     },[products])
 
     return (
