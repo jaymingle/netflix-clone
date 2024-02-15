395617171bc226ff6e14ae0a24ab22ed67e455ae
Jay Mingle <jkawudi@gmail.com>
2024-02-15 00:55:49 +0000

    Changes

--- src/components/PlanScreen.jsx
+++ src/components/PlanScreen.jsx
@@ -1,8 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './PlanScreen.css'
 const PlanScreen = () => {
 
     const [products, setProducts] = useState([])
+
+    useEffect(() => {
+
+    },[products])
 
     return (
         <div className="planScreen">
