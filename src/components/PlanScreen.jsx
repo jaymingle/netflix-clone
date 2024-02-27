2d13dceea8636a6bedf044e8680b98b62c8fbdcc
Jay Mingle <jkawudi@gmail.com>
2024-02-27 11:03:49 +0000

    Changes

--- src/components/PlanScreen.jsx
+++ src/components/PlanScreen.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './PlanScreen.css'
-import db, {empty_db} from "../../firebase.js";
+import db from "../../firebase.js";
 import { collection, query, where, doc, getDocs, getDoc, collectionGroup, getFirestore } from 'firebase/firestore';
 
 
@@ -10,25 +10,16 @@ const PlanScreen = () => {
 
     const [products, setProducts] = useState([])
 
-    console.log("My DB",db)
-    console.log("EMPTY DB",empty_db)
-
-
     useEffect(() => {
 
         const fetchProducts = async () => {
-            console.log('Fetching products...');
             try {
-                console.log('Hollaaaa')
                 const q = query(collection(db, 'products'), where('active', '==', true));
                 const querySnapshot = await getDocs(q)
-                console.log('getData')
-                console.log('Query snapshot:', querySnapshot.docs.length);
                 const productsData = {};
                 querySnapshot.forEach((productDoc) => {
                     productsData[productDoc.id] = productDoc.data();
                 });
-                console.log('Products data:', productsData);
                 setProducts(productsData);
             } catch (error) {
                 console.error('Error fetching products:', error);
